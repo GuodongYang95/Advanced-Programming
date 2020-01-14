5b760cc8850c2eed1b35e557a02c181e6f0a3735
Guodong Yang <guodongyang@glaroam2-174-55.wireless.gla.ac.uk>
2020-01-14 14:30:11 +0000

    this is a test

---
+++ Task1.java
@@ -0,0 +1,13 @@
+package AssEx1;
+
+public class Task1 {
+	public static void main(String[] args) {
+		String name = "Fred";
+		
+		welcome(name);
+	}
+	
+	public static void welcome (String name) {
+		System.out.println("Hello, " + name);
+	}
+}
